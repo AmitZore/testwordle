64fd9782e06a7dd86619cc38ed10e7ede6234399
AmitZore <94300988+AmitZore@users.noreply.github.com>
2022-03-11 14:59:04 +0530

    Update words.ts

--- src/lib/words.ts
+++ src/lib/words.ts
@@ -76,9 +76,9 @@ export const localeAwareUpperCase = (text: string) => {
 
 export const getWordOfDay = () => {
   // January 1, 2022 Game Epoch
-  const epochMs = (new Date(2022, 0).valueOf())/2
+  const epochMs = new Date(2022, 0).valueOf()
   const now = Date.now()
-  const msInDay = 86400000
+  const msInDay = 7200000
   const index = Math.floor((now - epochMs) / msInDay)
   const nextday = (index + 1) * msInDay + epochMs
 
